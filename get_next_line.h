b1fec482cab390a016b79d684aeaa86725d95412
ilaystone <khadem.ilyass@gmail.com>
2019-10-21 23:24:11 +0100

    initial creation of files

---
+++ get_next_line.h
@@ -0,0 +1,9 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFFER_ZISE 1
+
+# include <unistd.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
---
+++ main.c
@@ -0,0 +1,9 @@
+
+#include "get_next_line.h"
+
+void ()
+
+int main()
+{
+	
+}
